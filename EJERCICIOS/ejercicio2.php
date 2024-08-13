81815decbe51ef5fa8e5c007e13c7ee8502cf895
SEB-ALB <giladitas@gmail.com>
2024-08-12 22:26:13 -0300

    update

---
+++ EJERCICIOS/ejercicio2.php
@@ -0,0 +1,27 @@
+<?php
+
+    $nombre = $_POST['txtNombre'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        Nombre: 
+        <input type="text" name="txtNombre" id="User">ç
+        <input type="submit" value="Enviar ">
+    </form>
+    <form action="ejercicio2.php" method="get">
+        <label for="name">Apellido</label>
+            <input type="text" name="lastname" id="">
+    </form>
+
+    Hola mundooo
+</body>
+</html>
